05b497fdd35da84cf5ab4fffa18b55ddff565ac1
Jonaspng <jonaspng@hotmail.com>
2024-12-07 16:18:57 +0800

    feat(ability): update material abillity
    - only course owner or manager will be allowed to manage text chunks

--- app/models/components/course/materials_ability_component.rb
+++ app/models/components/course/materials_ability_component.rb
@@ -8,6 +8,8 @@ module Course::MaterialsAbilityComponent
       allow_upload_materials
       allow_staff_read_materials if course_user.staff?
       allow_teaching_staff_manage_materials if course_user.teaching_staff?
+      disallow_text_chunking if course_user.teaching_staff?
+      manage_text_chunking if course_user.manager_or_owner?
     end
 
     disallow_superusers_change_root_and_linked_folders
@@ -43,6 +45,16 @@ module Course::MaterialsAbilityComponent
     can :upload, Course::Material::Folder, { course_id: course.id }.
       reverse_merge(can_student_upload: true)
     can :manage, Course::Material, creator: user
+  end
+
+  def manage_text_chunking
+    can :create_text_chunks, Course::Material, material_course_hash
+    can :destroy_text_chunks, Course::Material, material_course_hash
+  end
+
+  def disallow_text_chunking
+    cannot :create_text_chunks, Course::Material, material_course_hash
+    cannot :destroy_text_chunks, Course::Material, material_course_hash
   end
 
   def allow_staff_read_materials
